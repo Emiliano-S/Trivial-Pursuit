20bfe81230c73cffcc2fa453943eeafccc42aeec
Emiliano Scanga <scangaemiliano@gmail.com>
2022-11-10 12:26:26 +0100

    Fix quot on questions

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -6,40 +6,48 @@ import { GameService } from 'src/app/service/game.service';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
   user: any = null;
   questions: any = null;
 
-  constructor(private game: GameService, private auth: AuthService) { }
+  constructor(private game: GameService, private auth: AuthService) {}
 
   ngOnInit(): void {
     this.user = this.auth.getUser();
-    console.log(this.user);
-    this.game.getRequests(this.user.category, this.user.difficulty).subscribe((data:any) => {
-
-     this.questions =  data.results.map((element: any) => {
-        return element = {...element, 'options': [element.correct_answer, ...element.incorrect_answers].sort(() => Math.random() - .5), 'answer': ''};
+    this.game
+      .getRequests(this.user.category, this.user.difficulty)
+      .subscribe((data: any) => {
+        this.questions = data.results.map((element: any) => {
+          element.question = element.question
+            .replace(/&quot;/g, '"')
+            .replace(/&#039;/g, "'");
+          return (element = {
+            ...element,
+            options: [
+              element.correct_answer,
+              ...element.incorrect_answers,
+            ].sort(() => Math.random() - 0.5),
+            answer: '',
+          });
+        });
       });
-      console.log(this.questions);
-    })
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
     let score: number = 0;
 
-    this.questions.forEach((question:any, index: number) => {
+    this.questions.forEach((question: any, index: number) => {
       question.answer = form.form.value[index];
     });
 
     this.questions.forEach((question: any) => {
-      if(question.answer === question.correct_answer){
+      if (question.answer === question.correct_answer) {
         score += 1;
       }
     });
 
     this.auth.setScore(score, this.questions);
   }
-
 }
